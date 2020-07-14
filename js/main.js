f5100eb8921943e43359b671e1908a4eaf879776
mango <32094915+annefromk@users.noreply.github.com>
2020-07-14 23:31:50 +0900

    Insta-Clone JS/CSS study
    1. scroll-event 만들기
    - 자연스러운 화면전환을 위해서 CSS에서는 transition을 씀

--- js/main.js
+++ js/main.js
@@ -4,12 +4,24 @@
 
 const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
+const sidebox = document.querySelector('.side_box');
 
 heart.addEventListener('click', function(){
 
     console.log('hit');
     heart.classList.toggle('on');
 });
+
+
+function resizeFunc(){
+    if(pageYOffset >= 10){
+
+        let calcWidth = (window.innerWidth *0.5) + 167;
+        console.log(window.innerWidth *0.5);
+
+        sidebox.style.left = calcWidth + 'px';
+    }
+}
 
 
 //스크롤 이벤트 (조건걸기)
@@ -20,8 +32,14 @@ function scrollFunc(){
 
     if(pageYOffset >= 10){
         header.classList.add('on');
+        sidebox.classList.add('on');
+
+        resizeFunc();
+        
     }else{
         header.classList.remove('on');
+        sidebox.classList.remove('on');
+        sidebox.removeAttribute('style');
     }
 }
 
